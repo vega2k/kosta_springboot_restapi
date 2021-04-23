5ca3dc16e02ca586807d59eb14b5e7cc2603bae2
vega2k <vega2k@naver.com>
2021-04-23 13:32:56 +0900

    Account 추가

--- src/main/java/com/myboot/restapi/accounts/AccountService.java
+++ src/main/java/com/myboot/restapi/accounts/AccountService.java
@@ -1,6 +1,13 @@
 package com.myboot.restapi.accounts;
 
+import java.util.Collection;
+import java.util.Set;
+import static java.util.stream.Collectors.toSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,7 +20,21 @@ public class AccountService implements UserDetailsService {
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		return null;
+		// orElseThrow(Supplier)
+		// Supplier의 T get()  리턴타입은 T extends Throwable 의 하위클래스이어야 한다.
+		// public <X extends Throwable> T orElseThrow​(Supplier<? extends X> exceptionSupplier) throws X extends Throwable
+		Account account = accountRepository.findByEmail(username)
+				                           .orElseThrow(() -> new UsernameNotFoundException(username));
+		//User(java.lang.String username, java.lang.String password, java.util.Collection<? extends GrantedAuthority> authorities)
+		return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
 	}
+
+	private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
+		//Set<AccountRole> -> Stream<AccountRole> -> Stream<SimpleGrantedAuthority> -> Collection<SimpleGrantedAuthority>
+		return roles.stream()
+				.map(accountRole -> new SimpleGrantedAuthority(accountRole.name()))
+				.collect(toSet());
+	}
+	
+	
 }
