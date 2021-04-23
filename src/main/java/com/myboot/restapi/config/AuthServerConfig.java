87f8808b0e61ddf20e591ac335e2b545f9bb2406
vega2k <vega2k@naver.com>
2021-04-23 16:01:42 +0900

    AuthServer Config 수정

--- src/main/java/com/myboot/restapi/config/AuthServerConfig.java
+++ src/main/java/com/myboot/restapi/config/AuthServerConfig.java
@@ -2,12 +2,16 @@ package com.myboot.restapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
+import com.myboot.restapi.accounts.AccountService;
 import com.myboot.restapi.common.AppProperties;
 
 @Configuration
@@ -15,24 +19,53 @@ import com.myboot.restapi.common.AppProperties;
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 	@Autowired
 	PasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	AppProperties appProperties;
+
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	AccountService accountService;
+
+	@Autowired
+	TokenStore tokenStore;
+
+	/*
+	 * Authorization Server의 EndPoint 설정
+	 * 1. 인증을 처리하는 AuthenticationManager 설정 ( authenticationManager() )
+	 * 2. DB에서 읽은 인증정보(username,password)를 User 객체에 담아서 반환해주는 UserDetailsService 설정
+	 * 3. Access Token을 저장해주는 TokenStore를 설정 (현재는 InMemory 에 token을 저장한다)  
+	 */
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager)
+				.userDetailsService(accountService)
+				.tokenStore(tokenStore);
+	}
 
 	// OAuth2 인증 서버 보안(Password) 정보를 설정
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.passwordEncoder(passwordEncoder);
 	}
-	
+
+	/*
+	 * 1. Client ID ( withClient() ) 와 Secret ID( secret())를 설정하고 
+	 * 2. Grant Type 설정 - Password와 Refresh_Token ( authorizedGrantTypes() )
+	 * 3. Scope 설정 - read / write (  scopes() )
+	 * 4. access token의 유효시간 설정 ( accessTokenValiditySeconds() 600초 10분 )
+	 * 5. refresh token의 유효시간 설정 ( refreshTokenValiditySeconds() 3600초 1시간 )
+	 */
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-	clients.inMemory()
-	 .withClient(appProperties.getClientId())
-	 .secret(this.passwordEncoder.encode(appProperties.getClientSecret()))
-	 .authorizedGrantTypes("password", "refresh_token")
-	 .scopes("read", "write")
-	 .accessTokenValiditySeconds(10 * 60)
-	 .refreshTokenValiditySeconds(6 * 10 * 60);
+		clients.inMemory()
+				.withClient(appProperties.getClientId())
+				.secret(this.passwordEncoder.encode(appProperties.getClientSecret()))
+				.authorizedGrantTypes("password", "refresh_token")
+				.scopes("read", "write")
+				.accessTokenValiditySeconds(10 * 60).refreshTokenValiditySeconds(6 * 10 * 60);
 	}
+
 }
--- src/main/java/com/myboot/restapi/config/SecurityConfig.java
+++ src/main/java/com/myboot/restapi/config/SecurityConfig.java
@@ -1,22 +1,49 @@
 package com.myboot.restapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+import com.myboot.restapi.accounts.AccountService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private AccountService accountService;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(accountService).passwordEncoder(passwordEncoder);
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.anonymous()
-			.and()
-		.formLogin()
-			.and()
-		.authorizeRequests()
-			.mvcMatchers(HttpMethod.GET, "/api/**").permitAll()
-			.anyRequest().authenticated();
+		http.anonymous().and().formLogin().and().authorizeRequests().mvcMatchers(HttpMethod.GET, "/api/**").permitAll()
+				.anyRequest().authenticated();
 	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
 }
