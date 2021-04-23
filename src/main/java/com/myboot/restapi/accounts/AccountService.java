0a27260b91f2682548bac25cf4fb1c2b9c6bdf90
vega2k <vega2k@naver.com>
2021-04-23 14:01:36 +0900

    Form 인증처리

--- src/main/java/com/myboot/restapi/accounts/AccountService.java
+++ src/main/java/com/myboot/restapi/accounts/AccountService.java
@@ -1,8 +1,9 @@
 package com.myboot.restapi.accounts;
 
+import static java.util.stream.Collectors.toSet;
+
 import java.util.Collection;
 import java.util.Set;
-import static java.util.stream.Collectors.toSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -11,30 +12,39 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService implements UserDetailsService {
 	@Autowired
 	AccountRepository accountRepository;
-	
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	public Account saveAccount(Account account) {
+		account.setPassword(this.passwordEncoder.encode(account.getPassword()));
+		return this.accountRepository.save(account);
+	}
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// orElseThrow(Supplier)
-		// Supplier의 T get()  리턴타입은 T extends Throwable 의 하위클래스이어야 한다.
-		// public <X extends Throwable> T orElseThrow​(Supplier<? extends X> exceptionSupplier) throws X extends Throwable
+		// Supplier의 T get() 리턴타입은 T extends Throwable 의 하위클래스이어야 한다.
+		// public <X extends Throwable> T orElseThrow​(Supplier<? extends X>
+		// exceptionSupplier) throws X extends Throwable
 		Account account = accountRepository.findByEmail(username)
-				                           .orElseThrow(() -> new UsernameNotFoundException(username));
-		//User(java.lang.String username, java.lang.String password, java.util.Collection<? extends GrantedAuthority> authorities)
+				.orElseThrow(() -> new UsernameNotFoundException(username));
+		// User(java.lang.String username, java.lang.String password,
+		// java.util.Collection<? extends GrantedAuthority> authorities)
 		return new User(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
 	}
 
 	private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
-		//Set<AccountRole> -> Stream<AccountRole> -> Stream<SimpleGrantedAuthority> -> Collection<SimpleGrantedAuthority>
-		return roles.stream()
-				.map(accountRole -> new SimpleGrantedAuthority(accountRole.name()))
-				.collect(toSet());
+		// Set<AccountRole> -> Stream<AccountRole> -> Stream<SimpleGrantedAuthority> ->
+		// Collection<SimpleGrantedAuthority>
+		return roles.stream().map(accountRole -> new SimpleGrantedAuthority(accountRole.name())).collect(toSet());
 	}
-	
-	
+
 }
--- src/main/java/com/myboot/restapi/config/AppConfig.java
+++ src/main/java/com/myboot/restapi/config/AppConfig.java
@@ -1,8 +1,19 @@
 package com.myboot.restapi.config;
 
+import java.util.Set;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.myboot.restapi.accounts.Account;
+import com.myboot.restapi.accounts.AccountRole;
+import com.myboot.restapi.accounts.AccountService;
 
 @Configuration
 public class AppConfig {
@@ -11,5 +22,28 @@ public class AppConfig {
 	public ModelMapper modelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
 		return modelMapper;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return new ApplicationRunner() {
+			@Autowired
+			AccountService accountService;
+
+			@Override
+			public void run(ApplicationArguments args) throws Exception {
+				Account account = Account.builder()
+									.email("user@email.com")
+									.password("user")
+									.roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+									.build();
+				accountService.saveAccount(account);
+			}
+		};
 	}
 }
